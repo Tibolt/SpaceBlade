c94ebd58766f60e729558a756ac8dccb6c02cb55
Tibolt <tibo.ak015@gmail.com>
2022-08-20 23:02:41 +0200

    player can shoot more bullets at once - testing

--- godot/src/player/Player.cs
+++ godot/src/player/Player.cs
@@ -1,160 +1,275 @@
 using Godot;
 using System;
+using System.Linq;
 
 public class Player : Ship
 {
-    public enum States
-    {
-        MOVE,
-        DEAD,
-    };
+	public enum States
+	{
+		MOVE,
+		DEAD,
+	};
 
-    public States State = States.MOVE;
-    public bool IsReloading = false;
-    [Export]
-    public float ReloadTime = 1.5f;
-    [Export]
-    public int NumberOfBullets = 5;
-    public int BulletsShooted {get;set;}
+	public States State = States.MOVE;
+	public bool IsReloading = false;
+	[Export]
+	public float ReloadTime = 1.5f;
+	[Export]
+	public int NumberOfBullets = 5;
+	public int BulletsShooted {get;set;}
+	[Export]
+	public int WeaponType = 1;
 
 
-    private PackedScene _bullet = (PackedScene)ResourceLoader.Load("res://src/player/PlayerBullet.tscn");
-    private Position2D _shootPoint {get; set;}
-    private Timer _reloadTimer {get;set;}
-    private GlobalVariables _globalVariables {set;get;}
-    public override void _Ready()
-    {
-        _shootPoint = GetNode<Position2D>("ShootPoint");   
-        _reloadTimer = GetNode<Timer>("ReloadTimer");
-        _globalVariables = GetNode<GlobalVariables>("/root/GlobalVariables");
+	private PackedScene _bullet = (PackedScene)ResourceLoader.Load("res://src/player/PlayerBullet.tscn");
+	private Position2D _shootPoint {get; set;}
+	private Timer _reloadTimer {get;set;}
+	private GlobalVariables _globalVariables {set;get;}
+	public override void _Ready()
+	{
+		_shootPoint = GetNode<Position2D>("ShootPoint");   
+		_reloadTimer = GetNode<Timer>("ReloadTimer");
+		_globalVariables = GetNode<GlobalVariables>("/root/GlobalVariables");
 
-        BulletsShooted = NumberOfBullets;
-        _globalVariables.SetPlayerHealth(Health);
+		BulletsShooted = NumberOfBullets;
+		_globalVariables.SetPlayerHealth(Health);
 
-    }
-    public override void _Input(InputEvent @event)
-    {
-        if(@event.IsActionPressed("reload"))
-            Reload();
-    }
+        InitShootPoints();
 
-    public override void _Process(float delta)
-    {
-        WrapEdges();
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if(@event.IsActionPressed("reload"))
+			Reload();
+	}
 
-        switch(State)
+	public override void _Process(float delta)
+	{
+		WrapEdges();
+
+		switch(State)
+		{
+			case States.MOVE:
+				MoveState(delta);
+				break;
+		}
+
+		if(Input.IsActionJustPressed("shoot") && !IsReloading)
+		{
+			// NumberOfBullets = 0 -> unlimited shooting
+			if(NumberOfBullets != 0 && BulletsShooted <= 0)
+			{
+				Reload();
+			}
+			else
+			{
+				InstanceBullet();
+				--BulletsShooted;
+			}
+		}
+        
+        var canRotate = false;
+        //rotate _shootPoint node
+        if(canRotate)
         {
-            case States.MOVE:
-                MoveState(delta);
-                break;
+            var rotation = _shootPoint.Rotation + 40 * delta;
+            _shootPoint.Rotate((float)rotation%360);
+        }
+	}
+	public Vector2 GetDirection()
+	{
+		return new Vector2(Input.GetActionStrength("right") - Input.GetActionStrength("left"), 0);
+	}
+
+	public void MoveState(float delta)
+	{
+		Vector2 direction = GetDirection();
+		direction = direction.Normalized();
+
+		if(direction != Vector2.Zero)
+		{
+			Velocity = Velocity.MoveToward(direction * MaxSpeed, Acceleration * delta);
+		}
+		else
+		{
+			Velocity = Velocity.MoveToward(Vector2.Zero, Friction * delta);
+		}
+
+		Velocity = MoveAndSlide(Velocity);
+
+	}
+
+	public void DeadState()
+	{
+		// play destroy animation
+	}
+
+	public void InstanceBullet()
+	{
+		/* var inits = FireMode(WeaponType); */
+		/* var length = inits[1]; */
+		/*         for(float i = 1; i <= inits[2]; i+=1) */
+		/*         { */
+		/*             float xPos = _shootPoint.GlobalPosition.x; */
+		/*             float yPos = _shootPoint.GlobalPosition.y; */
+
+		/*             var bulletInstance = _bullet.Instance<PlayerBullet>(); */
+		/*             bulletInstance.Position = new Vector2(xPos - length + i*length, yPos); */
+		/*             bulletInstance.Rotation = (inits[3] * 3.14F)/180; */
+
+		/*             GetParent().AddChild(bulletInstance); */
+		/*         } */
+        foreach(Node2D node in _shootPoint.GetChildren())
+        {
+            var bulletInstance = _bullet.Instance<PlayerBullet>();
+            GetParent().AddChild(bulletInstance);
+            bulletInstance.Position = node.GlobalPosition;
+            bulletInstance.Rotation = node.GlobalRotation;
+
         }
 
-        if(Input.IsActionJustPressed("shoot") && !IsReloading)
+	}
+    public void InitShootPoints()
+    {
+        var radius = 1f;
+        var startPos = -30f;
+        var rotateSpeed = 3f;
+        var interval = 0f;
+        var spawningCount = 3;
+
+        var step = Math.PI * 2 / spawningCount;
+        bool curve = false;
+
+        foreach(var i in Enumerable.Range(1, spawningCount))
         {
-            if(BulletsShooted <= 0)
+            var spawnPoint = new Node2D();
+            var pos = new Vector2(startPos + i*20, 0);
+            if(curve)
             {
-                Reload();
+                pos = new Vector2(radius, 0).Rotated((float)step * i);
+                spawnPoint.Rotation = pos.Angle();
             }
-            else
+            else if(WeaponType == 3)
             {
-                InstanceBullet();
-                --BulletsShooted;
+                // rotate first and last bullet 
+                if(i == 1)
+                {
+                    spawnPoint.Rotation = -10 * (float)Math.PI / 180f;
+                } 
+                if(i == 3)
+                {
+                    spawnPoint.Rotation = 10 * (float)Math.PI / 180f;
+                }
             }
-        }
-    }
-    public Vector2 GetDirection()
-    {
-        return new Vector2(Input.GetActionStrength("right") - Input.GetActionStrength("left"), 0);
-    }
-
-    public void MoveState(float delta)
-    {
-        Vector2 direction = GetDirection();
-        direction = direction.Normalized();
-
-        if(direction != Vector2.Zero)
-        {
-            Velocity = Velocity.MoveToward(direction * MaxSpeed, Acceleration * delta);
-        }
-        else
-        {
-            Velocity = Velocity.MoveToward(Vector2.Zero, Friction * delta);
-        }
-
-        Velocity = MoveAndSlide(Velocity);
-
-    }
-
-    public void DeadState()
-    {
-        // play destroy animation
-    }
-
-    public void InstanceBullet()
-    {
-        var bulletInstance = _bullet.Instance<PlayerBullet>();
-        bulletInstance.Position = _shootPoint.GlobalPosition;
-
-        GetParent().AddChild(bulletInstance);
-    }
-
-    public void OnHurtboxAreaEntered(Hitbox hitbox)
-    {
-        _globalVariables.SetPlayerHealth(_globalVariables.GetPlayerHealth() - 1);
-    }
-
-    public void OnReloadTimerTimeout()
-    {
-        IsReloading = false;
-        BulletsShooted = NumberOfBullets;
-    }
-
-    public void OnItemCollectorAreaEntered(Hurtbox hurtbox)
-    {
-        var item = hurtbox.GetParent<Item>();
-        if(item != null)
-            ItemEffects(item.ItemName);
-    }
-
-    public void ItemEffects(string name)
-    {
-        switch(name)
+            else if(WeaponType == 4)
             {
-                case "Health":
-                    Health++;
-                    GD.Print("Health: " + Health);
-                    break;
-                case "Armor":
-                    break;
-                case "Speed":
-                    Acceleration += 10;
-                    GD.Print("Acceleration: " + Acceleration);
-                    break;
-                case "Bullet":
-                    NumberOfBullets++;
-                    break;
-                case "Coin":
-                    GlobalVariables.Money += 10;
-                    break;
-                case "Skull":
-                    Acceleration -= 20;
-                    break;
-                default:
-                    break;
+                // rotate first and last bullet 
+                if(i == 1)
+                {
+                    spawnPoint.Rotation = -10 * (float)Math.PI / 180f;
+                } 
+                if(i == 4)
+                {
+                    spawnPoint.Rotation = 10 * (float)Math.PI / 180f;
+                }
             }
-    }
 
-    public void WrapEdges()
-    {
-        if(Position.x > GlobalVariables.ScreenRight)
-            Position = new Vector2(GlobalVariables.ScreenLeft, Position.y);
-        if(Position.x < GlobalVariables.ScreenLeft)
-            Position = new Vector2(GlobalVariables.ScreenRight, Position.y);
-    }
 
-    public void Reload()
-    {
-        _reloadTimer.Start(ReloadTime);
-        IsReloading = true;
+            spawnPoint.Position = pos;
+
+            _shootPoint.AddChild(spawnPoint);
+        }
     }
+	public float[] FireMode(int weapon)
+	{
+		int fireLoop = WeaponType;
+		float yPos = _shootPoint.GlobalPosition.y;
+		// for _shootPoint
+		// xStartPos, length bettwen, number of shootPoints, rotation
+		float[] positions = {0, 0, 0, 0};
+		switch(weapon)
+		{
+			case 1:
+				/* _shootPoint.Position = new Vector2(0, yPos); */
+				positions = new float[] {0 ,0, 1, 0};
+				break;
+			case 2:
+				/* _shootPoint.Position = new Vector2(-2, yPos); */
+				positions = new float[] {-3 ,6, 2, 0};
+				break;
+			case 3:
+				/* _shootPoint.Position = new Vector2(-5, yPos); */
+				positions = new float[] {-5 ,5, 3, 5};
+				break;
+			case 4:
+				/* _shootPoint.Position = new Vector2(-5, yPos); */
+				positions = new float[] {-5 ,2.5F, 4, 2};
+				break;
+		}
+		return positions;
+	}
+
+	public async void OnHurtboxAreaEntered(Hitbox hitbox)
+	{
+		_globalVariables.SetPlayerHealth(_globalVariables.GetPlayerHealth() - 1);
+
+		this.Modulate = new Color(Color.Color8(255, 120, 120));
+		await ToSignal(GetTree().CreateTimer((float)0.15), "timeout");
+		this.Modulate = new Color(Color.Color8(255, 255, 255));
+	}
+
+	public void OnReloadTimerTimeout()
+	{
+		IsReloading = false;
+		BulletsShooted = NumberOfBullets;
+	}
+
+	public void OnItemCollectorAreaEntered(Hurtbox hurtbox)
+	{
+		var item = hurtbox.GetParent<Item>();
+		if(item != null)
+			ItemEffects(item.ItemName);
+	}
+
+	public void ItemEffects(string name)
+	{
+		switch(name)
+			{
+				case "Health":
+					Health++;
+					GD.Print("Health: " + Health);
+					break;
+				case "Armor":
+					break;
+				case "Speed":
+					Acceleration += 10;
+					GD.Print("Acceleration: " + Acceleration);
+					break;
+				case "Bullet":
+					NumberOfBullets++;
+					break;
+				case "Coin":
+					GlobalVariables.Money += 10;
+					break;
+				case "Skull":
+					Acceleration -= 20;
+					break;
+				default:
+					break;
+			}
+	}
+
+	public void WrapEdges()
+	{
+		if(Position.x > GlobalVariables.ScreenRight)
+			Position = new Vector2(GlobalVariables.ScreenLeft, Position.y);
+		if(Position.x < GlobalVariables.ScreenLeft)
+			Position = new Vector2(GlobalVariables.ScreenRight, Position.y);
+	}
+
+	public void Reload()
+	{
+		_reloadTimer.Start(ReloadTime);
+		IsReloading = true;
+	}
 }
--- godot/src/player/PlayerBullet.cs
+++ godot/src/player/PlayerBullet.cs
@@ -1,55 +1,67 @@
 using Godot;
 using System;
 
-public class PlayerBullet : Node2D
+public class PlayerBullet : KinematicBody2D
 {
-    [Export]
-    public Vector2 Direction = Vector2.Up;
-    [Export]
-    public float MaxSpeed {get; set;}
-    [Export]
-    public float Acceleration {get; set;}
-    [Export]
-    public int Damage {get; set;}
-    [Export]
-    public bool IsPlayerBullet {get;set;}
+	[Export]
+	public Vector2 Direction = Vector2.Up;
+	[Export]
+	public float MaxSpeed {get; set;}
+	[Export]
+	public float Acceleration {get; set;}
+	[Export]
+	public int Damage {get; set;}
+	[Export]
+	public bool IsPlayerBullet {get; set;}
+	public Vector2 Velocity {get; set;}
 
 
-    public override void _Ready()
-    {
-        Visible = true;
-    }
+	public override void _Ready()
+	{
+		Visible = true;
+	}
 
-    public override void _Process(float delta)
-    {
-        Destory();
-        GlobalPosition += Direction * MaxSpeed * delta;
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+		Destory();
+		/* GlobalPosition += Direction * MaxSpeed * delta; */
 
-    public void Destory()
-    {
-        if(IsPlayerBullet)
-        {
-            if(Position.y <= GlobalVariables.ScreenTop)
-                QueueFree();
-        }
-        else
-        {
-            if(Position.y >= GlobalVariables.ScreenBottom)
-                QueueFree();
-        }
-    }
+		Velocity = new Vector2(Direction).Rotated(this.Rotation);
+		Velocity = Velocity.Normalized() * MaxSpeed;
 
-    public void OnTimerTimeout()
-    {
-        // TODO: Delete Timer
-        // Destory();
-    }
+		Velocity = MoveAndSlide(Velocity);
 
-    public void OnHitboxAreaEntered(Hurtbox hurtbox)
-    {
-        QueueFree();
-    }
+		
+	}
+
+	public void Destory()
+	{
+		if(IsPlayerBullet)
+		{
+			if(Position.y <= GlobalVariables.ScreenTop)
+				QueueFree();
+		}
+		else
+		{
+			if(Position.y >= GlobalVariables.ScreenBottom)
+				QueueFree();
+		}
+		if(Position.x >= GlobalVariables.ScreenRight)
+			QueueFree();
+		else if(Position.x <= GlobalVariables.ScreenLeft)
+			QueueFree();
+	}
+
+	public void OnTimerTimeout()
+	{
+		// TODO: Delete Timer
+		/* QueueFree(); */
+	}
+
+	public void OnHitboxAreaEntered(Hurtbox hurtbox)
+	{
+		QueueFree();
+	}
 
 
 }
